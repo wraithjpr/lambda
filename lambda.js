99786b47b867d146c48f91ae7da8c11e64f88384
wraithjpr <james.p.r.wraith@gmail.com>
2017-12-16 19:01:41 +0000

    Added syntax of Lambda expressions as comments.

--- lambda.js
+++ lambda.js
@@ -1,7 +1,46 @@
-// lambda character lower case (λ) in Linux is: ctrl+shift+U 03BB
-// lambda character upper case (Λ) in Linux is: ctrl+shift+U 039B
+/*
+ * My experiments with lambda calculus
+ * Sources:
+ * Michaelson, Greg; An Introduction to Functional Programming Through Lambda Calculus; Dover; 2011
+ */
+/*
+ * λ Expressions
+ * -------------
+ * <expression> ::= <name> | <function> | <application>
+ * <name> ::= any sequence of non-blank characters
+ * <function> ::= λ<name>.<body>
+ * <body> ::= <expression>
+ * <application> ::= (<function expression> <argument expression>)
+ * <function expression> ::= <expression>
+ * <argument expression> ::= <expression>
+ *
+ * def <name> = <function>
+ *
+ */
+/* A λ function is an abstraction over a λ expression. The λ precedes and introduces a name used for abstraction. The name is called the function's 'bound variable'. The '.' separates the name from the expression in which the abstraction with that name takes place. This expression is called the function's 'body'.
+ * A function application specialises an abstraction by providing a value for the name. The function expression contains the abstraction to be specialised with the argument expression. A function application is also known as a 'bound pair' and the function expression is said to be 'applied to' the argument expression.
+ * We may define a name/function association, a 'def'. For convenience and clarity, we can use the <name> in expressions to stand for the <function>
+ */
+/*
+ * Beta Reduction
+ * --------------
+ * A β reduction is the replacement of a bound variable with an argument in a function body.
+ *   (<name> <argument>) == (<function> <argument>)
+ *   (<function> <argument>) => <expression>
+ */
+/*
+ * beta character lower case (β) in Linux is: ctrl+shift+U 03B2
+ * beta character upper case (Β) in Linux is: ctrl+shift+U 0392
+ * lambda character lower case (λ) in Linux is: ctrl+shift+U 03BB
+ * lambda character upper case (Λ) in Linux is: ctrl+shift+U 039B
+ */
 
-// def identity = lamda λx.x
+/*
+ * My Lambda Functions
+ * -------------------
+ */
+
+// def identity = λx.x
 const identity = x => x;
 
 // def self_apply = λs.(s s)
