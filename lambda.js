5134a6854fcddbb5c7d8d0f5896e8adf1aa25f6b
wraithjpr <james.p.r.wraith@gmail.com>
2017-12-29 17:30:20 +0000

    Added natural numbers: successor, predecessor, iszero functions.

--- lambda.js
+++ lambda.js
@@ -58,19 +58,30 @@ const select_second = first => second => second;
 // def make_pair = λfirst.λsecond.λfunc.((func first) second)
 const make_pair = first => second => func => func(first)(second);
 
+// Helper for observing results of applying functions in node.js
+makeNodeInspectHelper = fxn => fxnLabel => () => `${fxnLabel} as [${typeof fxn}: ${fxn.name}]`;
+ 
+/* ==============================================================================================================
+ * B O O L E A N S
+ * ---------------
+ */
+
 // def true = select_first
 const TRUE = select_first;
-TRUE.inspect = () => `TRUE as [${typeof TRUE}: ${TRUE.name}]`;
+//TRUE.inspect = () => `TRUE as [${typeof TRUE}: ${TRUE.name}]`;
+TRUE.inspect = makeNodeInspectHelper(TRUE)('TRUE');
 
 // def false = select_second
 const FALSE = select_second;
-FALSE.inspect = () => `FALSE as [${typeof FALSE}: ${FALSE.name}]`;
+//FALSE.inspect = () => `FALSE as [${typeof FALSE}: ${FALSE.name}]`;
+FALSE.inspect = makeNodeInspectHelper(FALSE)('FALSE');
 
 /*
  * λe1.λe2.λc.((c e1) e2) implements the conditional (ternary) operator in JavaScript, condition ? expr1 : expr2
  * If c is true, then it is select_first and ((c e1) e2) == ((select_first e1) e2) => ... => e1. 
  * If c is false, then it is select_second and ((c e1) e2) == ((select_second e1) e2) => ... => e2. 
  */
+
 // def cond = λe1.λe2.λc.((c e1) e2)
 const cond = make_pair;
 
@@ -83,6 +94,64 @@ const AND = x => y => x(y)(FALSE);
 //def or = λx.λy.(((cond true) y) x) => ... => λx.λy.((x true) y)
 const OR = x => y => x(TRUE)(y);
 
+/* ==============================================================================================================
+ * N A T U R A L  N U M B E R S
+ * ----------------------------
+ * The approach is to define natural numbers (i.e. non-negative integers) as successors of zero, and to define zero as the identity function.
+ */
 
+// def zero = identity
+const zero = identity;
 
+/*
+ * A successor function creates numbers.
+ * For any arbitrary number, n, then n+1 is represented by the function λs.((s false) n)
+ *
+ * 1 = successor of 0
+ * 2 = successor of 1 = successor of successor of 0
+ * 3 = successor of 2 = successor of successor of successor of 0
+ */
+
+// def succ = λn.λs.((s false) n)
+const succ = n => s => s(FALSE)(n);
+
+const ZERO  = zero;
+const ONE   = succ(ZERO);
+const TWO   = succ(ONE);
+const THREE = succ(TWO);
+const FOUR  = succ(THREE);
+const FIVE  = succ(FOUR);
+const SIX   = succ(FIVE);
+const SEVEN = succ(SIX);
+const EIGHT = succ(SEVEN);
+const NINE  = succ(EIGHT);
+const TEN   = succ(NINE);
+
+/*
+ * A number is represented by a function with an argument, s, which may be used as a selector.
+ * Consider using select_first as that argument.
+ * Apply zero to select_first: (zero select_first) == (λx.x select_first) => select_first == true
+ * Apply a non-zero natural number, n+1, to select_first: (λs.((s false) n) select_first) => ((select_first false) n) => ... => false
+ * Therefore, applying any number to select_first gives us a test for zero.
+ */
+
+//def iszero = λn.(n select_first)
+const iszero = n => n(select_first);
+
+/*
+ * A number is represented by a function with an argument, s, which may be used as a selector.
+ * Consider using select_second as that argument.
+ * Apply a non-zero natural number, n+1, to select_second: (λs.((s false) n) select_second) => ((select_second false) n) => ... => n
+ * Therefore, applying any number to select_second strips off an application of the successor to give us the predecessor, n, and:
+ *
+ * def pred = λn.(n select_second)
+ *
+ * However, applying zero to select_second: (zero select_second) == (λx.x select_second) => select_second == false
+ * So then (pred zero) => ... => false, which is not a representation of a number, ie. undefined.
+ * Now, rather than accept that the predecessor of zero is undefined, we define the predecessor of zero to be zero and check for it using iszero to give a robust predecessor function.
+ *
+ */
+
+// def pred = λn.(((iszero n) zero) (n select_second))
+const pred = n => (iszero(n)(zero))(n(select_second));
 
